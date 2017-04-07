b4b919083c8d5f934cce9f9f1f30e5adc0c7d2dd
2019033 <riazm2001@outlook.com>
2017-04-07 09:00:52 -0400

    Password Verification and Creation of the Directory of User Credidentials
    Started work on a password verification function. I made the part of the
    function that makes sure a proper username and password is entered and
    started verifying passwords. I also made a dictionairy of usernames in
    relation to password

--- OriginalServer.py
+++ OriginalServer.py
@@ -6,6 +6,14 @@ import _thread
 IP = "10.10.18.234"
 port = 443
 Buffer = 1024
+applicationName="Assignment 2"
+connection=False
+passwordVer=False
+clientErrorMessage=""
+global username
+username=""
+global password
+password=""
 
 
 #Beginning search for clients
@@ -14,6 +22,51 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((IP,port))
 s.listen(5)
 conn, addr = s.accept()
+connection=True
+
+
+#this function will authenticate the username and password of the user
+def passwordVerification():
+
+    #waiting for the password to be sent
+    while True:
+        # waiting for the client to send the username and password
+        username, password = conn.recv(Buffer)
+
+        #checking if the string sent by the client starts with '@'. The client attaches an '@' sign to all usernames and
+        #password
+        if username[0]!="@":
+            clientErrorMessage=="notExpectedUsername"
+
+            #sending an error message to the client letting them know why they have been denied access to the chat systems
+            conn.send(clientErrorMessage.encode('utf-8'))
+            pass
+
+        #checking if a password has been sent by seeing if it has the '@' sign
+        if password[0]!='@':
+            clientErrorMessage == "notExpectedPassword"
+
+            # sending an error message to the client letting them know why they have been denied access to the chat systems
+            conn.send(clientErrorMessage.encode('utf-8'))
+            pass
+
+        if username[0]=='@' and password[0]== '@':
+            # if both a username and a password has been sent to the server than it will continue with the verification process
+            #this will break out of the while loop
+            break
+
+
+        #this is for debugging purposes
+
+        print("The user who is trying to connect:",username)
+        print("The password they have entered:",password)
+
+
+        #we will now call up our username and password dictionairy to verify the users credidentials
+
+
+
+
 
 #Function for receiving messages from client
 def Messages(Buffer):
