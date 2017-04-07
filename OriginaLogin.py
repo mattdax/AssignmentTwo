12c0c9d4ba49fcc5741f8900735ba1f2924d7192
mattdax <mjdaxner@gmail.com>
2017-04-07 12:18:19 -0400

    Added more stuffs

--- OriginaLogin.py
+++ OriginaLogin.py
@@ -1,4 +1,4 @@
-from tkinter import *
+from tk\inter import *
 import socket
 import time
 from _thread import start_new_thread
@@ -7,24 +7,15 @@ port = 443
 buffer = 1024
 class Client():
     def __init__(self):
-        self.bootup = Tk()
-
-        # Setup variables
-        self.bootup.geometry("500x100")
-        self.bootup.title("Assignment 2")
-
-        # Connecting Label
-        self.firstText = Label(self.bootup,text = "Connecting To Server...\nPlease Wait",font = ('Helvetica', 28))
-        self.firstText.pack()
-        time.sleep(2)
         try:
             self.server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
             self.server.connect((host,port))
+            self.loginScreen()
         except ConnectionRefusedError:
-            #Destroys other window, may not even popup
-            self.bootup.destroy()
+
             #Creates error window
             self.error = Tk()
+
             #Error window setups
             self.error.geometry("300x100")
             self.error.title("Assignment 2")
@@ -33,19 +24,19 @@ class Client():
             self.errorMessage = Label(self.error,text = "Could not connect to server\n Please try again later.",font = ('Helvetica', 14))
             self.errorMessage.pack()
 
-            self.quitbutton = Button(self.error,text = "Ok",font = ('Helvetica', 10))
-            self.quitbutton.pack(side = BOTTOM)
-
             #Error window loop
             self.error.mainloop()
 
-        #Main Loop
-        self.bootup.mainloop()
+    def loginScreen(self):
+        self.screenA = Tk()
 
-    def connecting(self):
-        time.sleep(2)
+        self.screenA.geometry("250x200")
+        self.screenA.title("Assignment 2")
+
+        self.account = Button(self.screenA,text = "Do you have an account already?",font = ('Helvetica', 14))
+        self.account.pack(side = TOP)
 
 
 
 
-Client().connecting()
+Client()
