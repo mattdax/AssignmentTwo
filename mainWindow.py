16bba08773c7983eb157f5ed8177e7fc2ab1db02
2019033 <riazm2001@outlook.com>
2017-05-23 21:43:40 -0400

    Finished the Main User Connection Window
    Except for a tiny variable bit, i have finished the window that will
    allow users to pick what other users they would like to chat with

--- mainWindow.py
+++ mainWindow.py
@@ -1,45 +1,71 @@
 from tkinter import *
+import _thread
+import time
 
 def GUI():
-    class Window():
+    class Gui(Frame):
         def __init__(self):
             #creating the window and setting its characteristics
-            self.Window= Tk()
-            self.Window.geometry("400x400")
+            # Initialise the frame
+            Frame.__init__(self)
+            self.grid()
+
+
+            self.createWidgets()
+
+        def createWidgets(self):
 
             #creating a list of people who the client can connect to and letting them pick which one they want to connect to
-            self.header = Label(self.Window, bd=0, bg="white", height="1", width="37", font="Arial",text='Connect To A User:')
+            self.header = Label(self, bd=0, bg="white", height="1", width="37", font="Arial",text='Connect To A User:')
             self.header.grid(row=0,column=0,columnspan=5)
 
 
 
-            self.user1=Label(self.Window, bd=0,font="Arial",text="hey")
+            self.user1=Label(self, bd=0,font="Arial",text="hey")
             self.user1.grid(row=1, column=1, sticky= W)
 
-            userlist=['riaz','matt']
+            userlist=['riaz','matt','james','richard']
             userLabelList=[]
+            userButtonList=[]
 
             for i in range(0, len(userlist),1):
-                userLabelList.append(Label(self.Window, bd=0,font="Arial",text=userlist[i]))
+                userLabelList.append(Label(self, bd=0,font="Arial",text=userlist[i]))
                 userLabelList[i].grid(row=i+1,column=1, sticky= W)
-                self.connectButton1 = Button(self.Window, text="Connect", command=self.connect(i))
-                self.connectButton1.grid(row=i+1, column=4, columnspan=1, sticky=W)
 
-            self.refresh=Button(self.Window, text='Refresh', command= self.refresh())
-            self.refresh.grid(row=4, column=4, columnspan=1, sticky=W)
+                userButtonList.append(Button(self, text="Connect", command=lambda:self.buttonCallBack))
+                userButtonList[i].bind(userLabelList[i])
+                userButtonList[i].grid(row=i+1, column=4, columnspan=1, sticky=W)
+
+            #command=lambda:self.connect()
+
+
+
+            self.refreshButton=Button(self, text='Refresh', command=self.refresh)
+            self.refreshButton.grid(row=12, column=2, columnspan=4, sticky=W)
 
             # looping the window so that it will continue to stay open
             #self.text1.delete(1.0, END)
 
-            self.Window.mainloop()
-
         def refresh(self):
-            print("hi")
+            print('Refreshed')
+            app.destroy()
+            GUI()
 
-        def connect(self, user):
+        def buttonCallBack(self,event):
+            print("hello")
+            mybutton = event.widget
+            text_at_row_col = mybutton["text"]
+
+            print(text_at_row_col)
+
+        def connect(self,user):
             #open the chat window
-            None
-    Window()
+            print(user)
+            app.destroy()
+
+
+    app=Gui()
+    app.mainloop()
 
 
 GUI()
