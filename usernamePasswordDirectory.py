20bb4836bc23e3f0c0ab5258bafbd65f74226607
mattdax <mjdaxner@gmail.com>
2017-05-24 02:23:51 -0700

    FIle Cleanup
    Removed old useles files that shouldnt be there

--- usernamePasswordDirectory.py
+++
@@ -1,4 +0,0 @@
-#setting up a directory of usernames and their corresponding passwords so that it can be referenced during the login process
-
-userPassList={'riazm2001':'firstuser',
-              'mattdax':'seconduser'}
